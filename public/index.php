51f2aad59f5e6222cf0a31bcc8479299b628a412
eric <eric.scheffer.pro@gmail.com>
2020-04-29 09:17:50 +0200

    1er commit

---
+++ public/index.php
@@ -0,0 +1,63 @@
+<?php
+
+use App\Tools\DevTools;
+use App\Tools\LibsLoader;
+use App\Tools\DatabaseTools;
+
+
+$loader = require '../vendor/autoload.php';
+var_dump($loader);
+//instancier et appeller les librairies externes
+$libsLoader = new LibsLoader();
+$libsLoader->loadLibraries();
+
+//instancier un devTool
+$tools = new DevTools();
+
+//instancier un tool pour pouvoir utiliser la BDD
+$dbTools = new DatabaseTools("mysql", "fleuriste", "root", "root");
+
+
+
+
+
+echo ('coucou');
+?>
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+
+<h4>Liste des plantes</h4>
+    <table class="table table-dark">
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Prix</th>
+                <th>Catégorie</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php  { ?>
+                <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+
+</body>
+
+</html>
---
+++ src/Models/PlanteTools.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace App\Tools;
+
+
+class PlanteTools
+{
+
+   
+}
